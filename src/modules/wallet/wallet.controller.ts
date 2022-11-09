8ce6e0dc8dee190484def2a7f769a7abe574ff3a
Oluwaseun <ruliseun@gmail.com>
2022-11-09 18:02:30 +0100

    withdrawal logic updated

--- src/modules/wallet/wallet.controller.ts
+++ src/modules/wallet/wallet.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Request, Body, UseGuards } from '@nestjs/common';
 import { GetUser } from '../../common/decorators';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { WithdrawalDto } from './dtos/WithdrawDto';
 import { WalletService } from './wallet.service';
 
 @Controller('wallet')
@@ -16,7 +15,6 @@ export class WalletController {
 
     @UseGuards(JwtAuthGuard)
     @Post('/withdraw')
-    // withdraw(@Body() withdrawalDto: WithdrawalDto){
     withdraw(@Request() req: any, @GetUser() user){
         return this.walletService.withdraw(req.body.amount, user.id)
     }
